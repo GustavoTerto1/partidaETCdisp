21463bae91f8a18cd466ce12534c8e1f9fb5af19
20185147 <20185147@DSN1271158608.cfp127aluno.local>
2021-03-03 16:25:03 -0300

    projeto criado

---
+++ partidaETCdisp.X/main.c
@@ -0,0 +1,14 @@
+/*
+ * File:   main.c
+ * Author: 20185147
+ *
+ * Created on 3 de Março de 2021, 16:23
+ */
+
+
+#include <xc.h>
+#include "config.h"
+
+void main(void) {
+    return;
+}
---
+++ partidaETCdisp.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=partidaETCdisp.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/partidaETCdisp.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=partidaetcdisp.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/partidaetcdisp.x.tar
